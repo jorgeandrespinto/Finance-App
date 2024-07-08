44870c7531bf54dbd593594ffb4319d46ddbb190
Jorge Pinto <jorgepintovega97@gmail.com>
2024-07-08 12:13:03 -0500

    Initial Commit

---
+++ Finance App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance App
+//
+//  Created by Jorge Pinto on 7/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance App/Finance_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_AppApp.swift
+//  Finance App
+//
+//  Created by Jorge Pinto on 7/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Finance_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
